6a2baa3387fdeac66426edc770527d4f08372491
Atlas-2005 <61873548+Atlas-2005@users.noreply.github.com>
2021-09-16 12:04:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -91,3 +91,31 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.login(token);
+
+/**WebUI/
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+  res.status(200).send("WHATABYTE: Food For Devs");
+});
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
