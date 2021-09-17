a78cb6a44a07ab86eb0429fcb32ffdab3348cc53
Atlas-2005 <61873548+Atlas-2005@users.noreply.github.com>
2021-09-16 21:52:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ client.on('ready' , () =>{
 
 });
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://pogchamp-rythm-bot.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
