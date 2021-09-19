e7ca9759cafc47fc7b70ecfd8795805c21fa218f
Atlas-2005 <61873548+Atlas-2005@users.noreply.github.com>
2021-09-18 22:13:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,12 @@ for (const file of commandFiles) {
 
 console.log(client.commands);
 
-const player = new Player(client);
+const player = new Player(client, {
+  leaveOnEnd: true,
+  leaveOnEndCooldown: 300000,
+  leaveOnEmpty: true,
+  leaveOnEmptyCooldown: 120000
+});
 
 player.on('error', (queue, error) => {
   console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
