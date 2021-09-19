448b8d228f05c609e49d4eda5a03686e55ccd431
Atlas-2005 <61873548+Atlas-2005@users.noreply.github.com>
2021-09-18 22:02:23 -0400

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -45,9 +45,9 @@ module.exports = {
 
       const queue = await player.createQueue(interaction.guild, {
         metadata: interaction.channel,
-        leaveOnEnd:
-          player.client.config.leaveOnEnd &&
-          player.client.config.leaveOnEndTimeout == 0,
+//         leaveOnEnd:
+//           player.client.config.leaveOnEnd &&
+//           player.client.config.leaveOnEndTimeout == 0,
         leaveOnStop: false,
         leaveOnEmpty: player.client.config.leaveOnEmpty,
         leaveOnEmptyCooldown: player.client.config.leaveOnEmptyTimeout * 1000,
