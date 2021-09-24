c00f78620d99be49e10ffea6ac0407b9798031e0
Atlas-2005 <61873548+Atlas-2005@users.noreply.github.com>
2021-09-24 10:07:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
-client.login(token);
+client.login(token0,token1);
 // client.then(client.user.setPresence({status: "dnd"}));
 
 // console.log('Spectre Is Now Online!');
