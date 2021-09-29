5faabc2c6de6f42fe985da74e42c85480ec4b52a
Mailman <59939184+BobieJamJoe@users.noreply.github.com>
2021-09-29 17:41:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ module.exports = {
 // Uncomment settings that you want enabled
 
 // Fill discord token in if you are not using github secrets
-  var discord_token: 
+  var discord_token = ;
 
 // Set your prefix for the deploy command below
   "prefix": "!",
-  
+
 // Use the following section to setup your discord bot status
   "status": "tName+" | "+"Song Length - "+tProg, { type: 'LISTENING' }"
-  
+
 // DON'T TOUCH ANYTHING BELOW THIS!!!
   if (process.env.TOKEN && (discord_token =>0)) {
     "token": process.env.TOKEN
