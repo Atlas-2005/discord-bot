e7182fedef60050e7dfb7eafbe628d3be20d03a9
Mailman <59939184+BobieJamJoe@users.noreply.github.com>
2021-09-29 10:27:49 -0400

    Update disconnect.js

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -1,12 +1,12 @@
-module.exports = {
-  name: 'Disconnect',
-  description: 'Disconnect me from the voice channel!',
-    execute(interaction) {
-      message.guild.me.voice.channel.leave();
+// module.exports = {
+//   name: 'Disconnect',
+//   description: 'Disconnect me from the voice channel!',
+//     execute(interaction) {
+//       message.guild.me.voice.channel.leave();
     
-      return void interaction.reply({
-        content: `I was disconnected.`,
-        ephemeral: true,
-      });
-    },
-}
+//       return void interaction.reply({
+//         content: `I was disconnected.`,
+//         ephemeral: true,
+//       });
+//     },
+// }
