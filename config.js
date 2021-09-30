882e11f0d17eb912860d8ef094ed311cc905bd42
Atlas-2005 <61873548+Atlas-2005@users.noreply.github.com>
2021-09-30 08:30:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,19 +3,20 @@
 // Fill discord token in if you are not using github secrets. Otherwise keep it as "0"
   var discord_token = "0";
 
-// Leave this line 
-module.exports = {
-
 // Set your prefix for the deploy command below
-  "prefix": "!"
+  var prefix = "!";
 
 // Use the following section to setup your discord bot status
   // "status": tName+" | "+"Song Length - "+tProg, { type: 'LISTENING' }
 
 // DON'T TOUCH ANYTHING BELOW THIS!!!
   if (discord_token => 0) {
-    "token": process.env.TOKEN
+    var token = process.env.TOKEN
   } else {
-    "token": discord_token
+    var token = discord_token
   }
+
+module.exports = {
+  "prefix": prefix
+  "token": token
 };
